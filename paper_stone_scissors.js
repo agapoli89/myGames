9c250abae6d01d2c2a37ce07cdf9f134e0f5f490
agapoli89 <agapoli@gmail.com>
2020-09-06 17:45:20 +0200

    my first game

---
+++ paper_stone_scissors.js
@@ -0,0 +1,75 @@
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+}
+
+const game = {
+    yourChoice: '',
+    aiChoice: '',
+    whoWin: '',
+}
+
+const hands = document.querySelectorAll('.select img');
+const btn = document.querySelector('.start');
+
+//pierwsza funkcja - wybór gracza
+const handSelection = (e) => {
+    game.yourChoice = e.target.dataset.option;
+    console.log(game.yourChoice);
+    hands.forEach(hand => hand.style.boxShadow = '');
+    e.target.style.boxShadow = '0 0 0 4px yellow';
+}
+
+//druga funkcja - wybór komputera
+const aiHandSelection = () => {
+    const index = Math.floor(Math.random() * hands.length);
+    return hands[index].dataset.option;
+}
+
+//trzecia funkcja - porównanie wyników
+const checkResult = (player, ai) => {
+    if (player === ai) {
+        gameSummary.draws++;
+        return 'Remis';
+    } else if ((player === 'papier' && ai === 'kamień') || (player === 'kamień' && ai === 'nożyczki') || (player === 'nożyczki' && ai === 'papier')) {
+        gameSummary.wins++;
+        return 'Ty! :)';
+    } else {
+        gameSummary.losses++;
+        return 'Komputer :(';
+    }
+}
+
+//czwarta funkcja - wyświetlenie wyników
+const showResults = () => {
+    document.querySelector('[data-summary="your-choice"]').textContent = game.yourChoice;
+    document.querySelector('[data-summary="ai-choice"]').textContent = game.aiChoice;
+    document.querySelector('.numbers span').textContent = gameSummary.numbers;
+    document.querySelector('.wins span').textContent = gameSummary.wins;
+    document.querySelector('.losses span').textContent = gameSummary.losses;
+    document.querySelector('.draws span').textContent = gameSummary.draws;
+    document.querySelector('[data-summary="who-win"]').textContent = game.whoWin;
+}
+
+
+//funkcja sterująca
+const startGame = () => {
+    if (!game.yourChoice) {
+        return alert('Wybierz obraz!');
+    }
+
+    gameSummary.numbers++;
+    game.aiChoice = aiHandSelection();
+    console.log(game.aiChoice);
+
+    const gameResult = checkResult(game.yourChoice, game.aiChoice);
+    game.whoWin = gameResult;
+
+    showResults();
+}
+
+hands.forEach(hand => hand.addEventListener('click', handSelection));
+
+btn.addEventListener('click', startGame);
